82c5785b3f4c448943c11ce7702a16ea482ad2ea
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-08-28 11:11:53 -0400

    starting code

---
+++ client/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import MessagesList from './MessagesList';
+import store, { fetchMessages } from '../store';
+
+export default class Main extends Component {
+
+  componentDidMount () {
+    const thunk = fetchMessages();
+    store.dispatch(thunk);
+  }
+
+  render () {
+    return (
+      <div>
+        <Sidebar />
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/channels/:channelId" component={MessagesList} />
+            <Redirect to="/channels/1" />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
---
+++ client/store.js
@@ -0,0 +1,117 @@
+import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import socket from './socket';
+
+// INITIAL STATE
+
+const initialState = {
+  messages: [],
+  name: 'Reggie',
+  newMessageEntry: ''
+};
+
+// ACTION TYPES
+
+const UPDATE_NAME = 'UPDATE_NAME';
+const GET_MESSAGE = 'GET_MESSAGE';
+const GET_MESSAGES = 'GET_MESSAGES';
+const WRITE_MESSAGE = 'WRITE_MESSAGE';
+
+// ACTION CREATORS
+
+export function updateName (name) {
+  const action = { type: UPDATE_NAME, name };
+  return action;
+}
+
+export function getMessage (message) {
+  const action = { type: GET_MESSAGE, message };
+  return action;
+}
+
+export function getMessages (messages) {
+  const action = { type: GET_MESSAGES, messages };
+  return action;
+}
+
+export function writeMessage (content) {
+  const action = { type: WRITE_MESSAGE, content };
+  return action;
+}
+
+// THUNK CREATORS
+
+export function fetchMessages () {
+
+  return function thunk (dispatch) {
+    return axios.get('/api/messages')
+      .then(res => res.data)
+      .then(messages => {
+        const action = getMessages(messages);
+        dispatch(action);
+      });
+  }
+}
+
+export function postMessage (message) {
+
+  return function thunk (dispatch) {
+    return axios.post('/api/messages', message)
+      .then(res => res.data)
+      .then(newMessage => {
+        const action = getMessage(newMessage);
+        dispatch(action);
+        socket.emit('new-message', newMessage);
+      });
+  }
+
+}
+
+// REDUCER
+
+function reducer (state = initialState, action) {
+
+  switch (action.type) {
+
+    case UPDATE_NAME:
+      return {
+        ...state,
+        name: action.name
+      };
+
+    case GET_MESSAGES:
+      return {
+        ...state,
+        messages: action.messages
+      };
+
+    case GET_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, action.message]
+      };
+
+    case WRITE_MESSAGE:
+      return {
+        ...state,
+        newMessageEntry: action.content
+      };
+
+    default:
+      return state;
+  }
+
+}
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(
+    thunkMiddleware,
+    createLogger()
+  ))
+);
+
+export default store;
